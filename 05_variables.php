1d8c1d6bc483bba794ef11fc174485a37d812f29
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-08-29 12:06:50 +0530

    Add files via upload

---
+++ 05_variables.php
@@ -0,0 +1,8 @@
+<?php
+echo "Jaivik Parekh<br>";
+$name = "Jaivik";
+$income = 5000;
+
+echo " This guy is $name and his income is $income<br>";
+echo " his networth is $income"
+?>
---
+++ 06_more_on_variables.php
@@ -0,0 +1,12 @@
+<div class="container">
+<h2>more on variables</h2>
+<ul>
+    <li> made my jaivik</li>
+</ul>
+<p>
+
+    <?php
+        echo"more on variables";
+    ?>
+    </p>
+</div>
