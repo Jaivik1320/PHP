72d7eaa4882826e25d167809e70a3ecb9c193730
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-09-01 11:26:12 +0530

    Add files via upload

---
+++ 11_switchcase.php
@@ -0,0 +1,19 @@
+<?php
+
+$marks = 12;
+
+switch($marks){
+    case 12:
+        echo " your marks is 12 <br>";
+       
+    case 54:
+        echo " your marks is 54  <br>";
+        
+    case 44:
+        echo "your marks is 44 <br>";
+        
+    default:
+        echo "you are passed <br>";
+    }
+
+?>
---
+++ 12_loop.php
@@ -0,0 +1,14 @@
+<?php
+echo " While loops in php";
+echo '<br>';
+
+$i = 45;
+while($i < 55){
+	echo $i;
+	echo '<br>';
+	$i++;
+}
+
+
+
+?>
