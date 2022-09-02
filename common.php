9df6900b61786a8bc98b913e6420b78d4028c464
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-09-02 10:44:58 +0530

    PHP Country, State and Cities
    Program for Country,city and state list

---
+++ common.php
@@ -0,0 +1,23 @@
+<?php
+class Common
+{
+  public function getCountry($connection)
+  {
+      $mainQuery = "SELECT * FROM bird_countries";
+      $result1 = $connection->query($mainQuery) or die("Error in main Query".$connection->error);
+      return $result1;
+  }
+
+  public function getStateByCountry($connection,$countryId){
+      $query = "SELECT * FROM bird_states WHERE countryId='$countryId'";
+      $result = $connection->query($query) or die("Error in  Query".$connection->error);
+      return $result;
+  }
+
+  public function getCityByState($connection,$stateId){
+      $query = "SELECT * FROM bird_cities WHERE state_id='$stateId'";
+      $result = $connection->query($query) or die("Error in  Query".$connection->error);
+      return $result;
+  }
+}
+?>
