ec6030a4878d742f8a6d0d23e780e973dc4a421d
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-09-06 10:20:37 +0530

    Add files via upload

---
+++ mysql.php
@@ -0,0 +1,48 @@
+<?php
+INSERT INTO `countries` VALUES (1, 'USA', 1);
+INSERT INTO `countries` VALUES (2, 'Canada', 1);
+INSERT INTO `countries` VALUES (3, 'India', 1);
+INSERT INTO `countries` VALUES (4, 'Pakistan', 1);
+INSERT INTO `countries` VALUES (5, 'United Kingdom', 1);
+INSERT INTO `countries` VALUES (6, 'France', 1);
+INSERT INTO `countries` VALUES (7, 'Germany', 1);
+INSERT INTO `countries` VALUES (8, 'Japan', 1); 
+INSERT INTO `countries` VALUES (9, 'Australia', 1);
+
+ 
+INSERT INTO `states` VALUES (1, 1, 'New York', 1);
+INSERT INTO `states` VALUES (2, 1, 'Los Angeles', 1);
+INSERT INTO `states` VALUES (3, 2, 'British Columbia', 1);
+INSERT INTO `states` VALUES (4, 2, 'Torentu', 1);
+INSERT INTO `states` VALUES (5, 3, 'Gujarat', 1);
+INSERT INTO `states` VALUES (6, 3, 'Maharashtra', 1);
+INSERT INTO `states` VALUES (7, 3, 'Rajasthan', 1);
+INSERT INTO `states` VALUES (8, 3, 'J.K', 1);
+INSERT INTO `states` VALUES (9, 3, 'Uttar Pradesh', 1);
+INSERT INTO `states` VALUES (10, 1, 'california', 1);
+INSERT INTO `states` VALUES (11, 1, 'Boston', 1);
+INSERT INTO `states` VALUES (12, 1, 'Michigan', 1);
+INSERT INTO `states` VALUES (13, 1, 'Detroit', 1);
+INSERT INTO `states` VALUES (14, 1, 'Sanfrasciso', 1);
+INSERT INTO `states` VALUES (15, 2, 'Alberta', 1);
+INSERT INTO `states` VALUES (16, 2, 'Nova scotia', 1);
+INSERT INTO `states` VALUES (17, 2, 'ontario', 1);
+ 
+INSERT INTO `cities` VALUES (1, 2, 'Los Angales', 1);
+INSERT INTO `cities` VALUES (2, 1, 'New York', 1);
+INSERT INTO `cities` VALUES (3, 4, 'Toranto', 1);
+INSERT INTO `cities` VALUES (4, 3, 'Vancovour', 1);
+INSERT INTO `cities` VALUES (5, 3, 'Vadodara', 1);
+INSERT INTO `cities` VALUES (6, 3, 'Mumbai', 1);
+INSERT INTO `cities` VALUES (7, 3, 'Nagpur', 1);
+INSERT INTO `cities` VALUES (8, 3, 'ahemdabad', 1);
+INSERT INTO `cities` VALUES (9, 3, 'noidA', 1);
+INSERT INTO `cities` VALUES (10, 1, 'Silicon valley', 1);
+INSERT INTO `cities` VALUES (11, 1, 'newhaven', 1);
+INSERT INTO `cities` VALUES (12, 1, 'texas', 1);
+INSERT INTO `cities` VALUES (13, 2, 'edmonton', 1);
+INSERT INTO `cities` VALUES (14, 2, 'hamilton', 1);
+INSERT INTO `cities` VALUES (15, 3, 'Vancovour', 1);
+INSERT INTO `cities` VALUES (16, 3, 'Vancovour', 1);
+INSERT INTO `cities` VALUES (17, 3, 'Vancovour', 1);
+?>
