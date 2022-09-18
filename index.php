0cd7d4a27a2a513f2b410b39b764b484579c112b
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-09-18 12:17:15 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,101 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Country,States and Cities</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <style>
-    input[type=text], select {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-    }
-    input[type=submit] {
-        width: 100%;
-        background-color: #4CAF50;
-        color: white;
-        padding: 10px;
-        margin: 8px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    input[type=submit]:hover {
-        background-color: #45a049;
-    }
-
-    #box {
-        border-radius: 5px;
-        background-color: #f2f2f2;
-        padding: 20px;
-        margin: 0px 200px 0px 200px;
-    }
-</style>
-</head>
-<body>
-    
-<div class="jumbotron text-center">
-    <h1>Get City by State by Country</h1>
-    
-</div>
-<div id="box">
-    <form action="script.php" method="post">
-        <?php
-        include "config.php";
-        include_once "Common.php";
-        $common = new Common();
-        $countries = $common->getCountry($connection);
-        ?>
-        <label>Country <span style="color:red">*</span></label>
-        <select name="country" id="countryId" class="form-control" onchange="getStateByCountry();">
-            <option value="">Country</option>
-            <?php
-            if ($countries->num_rows > 0 ){
-                while ($country = $countries->fetch_object()) {
-                    $countryName = $country->name; ?>
-                    <option value="<?php echo $country->id; ?>"><?php echo $countryName;?></option>
-                <?php }
-            }
-            ?>
-        </select>
-        <label>State <span style="color:red">*</span></label>
-        <select class="form-control" name="state" id="stateId" onchange="getCityByState();" >
-            <option value="">State</option>
-        </select>
-
-        <label>City <span style="color:red">*</span></label>
-        <select class="form-control" name="city" id="cityDiv">
-            <option value="">City</option>
-        </select>
-
-       
-    </form>
-</div>
-<script>
-    function getStateByCountry() {
-        var countryId = $("#countryId").val();
-        $.post("ajax.php",{getStateByCountry:'getStateByCountry',countryId:countryId},function (response) {
-            var data = response.split('^');
-            $("#stateId").html(data[1]);
-        });
-    }
-    function getCityByState() {
-        var stateId = $("#stateId").val();
-        $.post("ajax.php",{getCityByState:'getCityByState',stateId:stateId},function (response) {
-            var data = response.split('^');
-            $("#cityDiv").html(data[1]);
-        });
-    }
-</script>
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
